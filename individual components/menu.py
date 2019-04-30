2966d11c60b457c1a1f2ff7ba7a781e6facaaf5e
nailcliper <ltayanami@hotmail.com>
2019-04-30 16:56:45 -0700

    Expanded basic machine

--- individual components/menu.py
+++ individual components/menu.py
@@ -9,9 +9,30 @@ class Menu():
     def tick(self):
         state = self.state
         
-        if(globals.test < 25):
-            globals.test += 1
-            print("Menu: "+str(globals.test))
+        #Transitions
+        if state == 0:
+            state = 1
+        elif state == 1:
+            if globals.test < 9:
+                state = 1
+                globals.test += 1
+            else:
+                state = 2
+        elif state == 2:
+            state = 2
+        else:
+            state = 0
         
+        #Actions
+        if state == 0:
+            pass
+        elif state == 1:
+            print("Menu: "+str(globals.test))
+        elif state == 2:
+            pass
+        else:
+            pass
+            
         self.state = state
+    #end def tick
 #end class Menu

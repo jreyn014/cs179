f729c38b63f139a285e365fc2897a0254c0e0382
nickeyu <nyu005@ucr.edu>
2019-05-21 20:48:47 +0000

    btooth changes

--- individual components/server_bluetooth_test.py
+++ individual components/server_bluetooth_test.py
@@ -1,27 +1,32 @@
 import bluetooth
-#import socket
-#hostMACAddress = "B8:27:EB:A6:9E7E" #Jesus
+import socket
+hostMACAddress = "B8:27:EB:A6:9E:7E" #Jesus
 #hostMACAddress = "B8:27:EB:AB:1C:2B" #Josh
 #hostMACAddress = "B8:27:EB:1A:E0:6F" #Nicke
-port = 3
-backlog = 1
+#hostMACAddress = ""
+port = 4
+backlog = 10
 size = 1024
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.bind((hostMACAddress, port))
 print("Listening")
 s.listen(backlog)
+
 try:
     client, clientInfo = s.accept()
     while 1:
-        print("%s connected", clientInfo)
-        data = client.recv(size)
+        print("Connected", clientInfo)
+	client.send("Hello")
+	data = client.recv(size)
         if not data:
-            print 'Wrong Data ' + data
+            print ("Wrong Data %s" % data)
 
-	print 'received: ' + data
+	print ("received: %s" % data)
+#	print ("Info: %s" % info)
 
-        echo = "Echo: " + data
-        s.send(str(echo))
+#        echo = info
+#        s.send(str(echo))
+
 except:
     print("Closing socket")
     #client.close()

38c183573fc54448596e1bad519e9f891c691c99
Jesus Reynoso <jreyn014@ucr.edu>
2019-05-23 13:35:19 -0700

    added missing parenthesis

--- individual components/server_bluetooth.py
+++ individual components/server_bluetooth.py
@@ -2,35 +2,35 @@ import bluetooth
 import subprocess
 
 def WaitForClient():
-	hostMACAddress = "B8:27:EB:A6:9E:7E" #Jesus
-	#hostMACAddress = "B8:27:EB:AB:1C:2B" #Josh
-	#hostMACAddress = "B8:27:EB:1A:E0:6F" #Nicke
-	#hostMACAddress = ""
-	subprocess.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
-	port = 4
-	backlog = 10
-	size = 1024
-	s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-	s.bind((hostMACAddress, port))
-	print("Listening")
-	s.listen(backlog)
-	try:
-    		client, clientInfo = s.accept()
-    		while 1:
-        		print("Connected", clientInfo)
-			client.send("You are connected" )
-			data = client.recv(size)
-        		if not data:
-            			print ("Wrong Data %s" % data)
+    hostMACAddress = "B8:27:EB:A6:9E:7E" #Jesus
+    #hostMACAddress = "B8:27:EB:AB:1C:2B" #Josh
+    #hostMACAddress = "B8:27:EB:1A:E0:6F" #Nicke
+    #hostMACAddress = ""
+    subprocess.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
+    port = 4
+    backlog = 10
+    size = 1024
+    s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    s.bind((hostMACAddress, port))
+    print("Listening")
+    s.listen(backlog)
+    try:
+            client, clientInfo = s.accept()
+            while 1:
+                print("Connected", clientInfo)
+                client.send("You are connected")
+                data = client.recv(size)
+                if not data:
+                        print ("Wrong Data %s" % data)
 
-			print ("received: %s" % data)
-	except:
-    		print("Closing socket")
-    		#client.close()
-    		s.close()
+                print ("received: %s" % data)
+    except:
+            print("Closing socket")
+            #client.close()
+            s.close()
 
 def main():
-	WaitForClient()
+    WaitForClient()
 
 if __name__ == "__main__":
-	main()
+    main()

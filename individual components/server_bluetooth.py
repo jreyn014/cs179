616b99b6dd7a98a847d193f5e815b3bf3f335b5e
nickeyu <nyu005@ucr.edu>
2019-05-28 22:05:40 +0000

    Bug fixing

--- individual components/server_bluetooth.py
+++ individual components/server_bluetooth.py
@@ -13,34 +13,33 @@ def recv():
 
 
 def WaitForClient():
-    #hostMACAddress = "B8:27:EB:A6:9E:7E" #Jesus
+	hostMACAddress = "B8:27:EB:A6:9E:7E" #Jesus
     #hostMACAddress = "B8:27:EB:AB:1C:2B" #Josh
-    hostMACAddress = "B8:27:EB:1A:E0:6F" #Nicke
-    #hostMACAddress = ""
-    subprocess.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
-    port = 4
-    backlog = 10
-    size = 1024
-    s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    s.bind((hostMACAddress, port))
-    print("Listening")
-    s.listen(backlog)
-    try:
-	    client, clientInfo = s.accept()
-            #while 1:
-	    print("Connected", clientInfo)
-	    threading.Thread(target=recv, daemon=True).start()
+    #hostMACAddress = "B8:27:EB:1A:E0:6F" #Nicke
+	#hostMACAddress = ""
+	subprocess.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
+	port = 4
+	backlog = 1
+	size = 1024
+	s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+	s.bind((hostMACAddress, port))
+	print("Listening")
+	s.listen(backlog)
+	try:
+		client, clientInfo = s.accept()
+		while 1:
+			print("Connected", clientInfo)
+	    #threading.Thread(target=recv, daemon=True).start()
 		#
-               # client.send("You are connected")
-                #data = client.recv(size)
-                #if not data:
-                #        print ("Wrong Data %s" % data)
-
-               # print ("received: %s" % data)
-    except:
-            print("Closing socket")
+			client.send("You are connected")
+			data = client.recv(size)
+			if not data:
+				print ("Wrong Data %s" % data)
+			print ("received: %s" % data)
+	except:
+		print("Closing socket")
             #client.close()
-            s.close()
+		s.close()
 
 def main():
     WaitForClient()

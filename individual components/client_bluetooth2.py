f046f572118ad4ef37bf449084b09eba1233d9f4
Josh Wilson <josh@ucrwpa-5-2-10-28-5-150.wnet.ucr.edu>
2019-05-30 13:52:23 -0700

    josh test

---
+++ individual components/client_bluetooth2.py
@@ -0,0 +1,44 @@
+import bluetooth
+#import socket
+
+def ConnectToHost():
+    print("performing inquiry...")
+    nearby_devices = bluetooth.discover_devices(lookup_names = True)
+    print("found %d devices" % len(nearby_devices))
+    #hostMACAddr = "B8:27:EB:A6:9E7E" #Jesus'
+    #hostMACAddr = "B8:27:EB:1A:E0:6F" #Nicke's
+    port = 4
+    s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    j = 0
+    try:
+        for addr, name in nearby_devices:
+                print( "%s - %s" % (addr, name))
+                if str(name) == ("PLUTO") or str(name) == ("Novy"):
+                        print ("Found Pi")
+                        s.connect((addr, port))
+                        print ("Connected")
+                        while 1:
+                           reply = s.recv(1024)
+                           print(reply)
+                           data = reply + 1
+                            #data = raw_input()
+                           if len(data) == 0:
+                                break
+                           s.send(data)
+                        #   print 'Sent DATA: ' + data
+                        #j = j + 1
+        print("Devices are not PLUTO")
+    except:
+        print("Error: Closing socket")
+        #client.close()
+        s.close()
+#s.close()
+
+def main():
+    ConnectToHost()
+    #s.close()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ individual components/server_bluetooth2.py
@@ -0,0 +1,57 @@
+import bluetooth
+import subprocess
+import threading
+
+global s
+
+def recv():
+	while 1:
+		data = s.recv(size)
+		if not data:
+			print("Wrong Data %s" % data)
+		print("received: %s" % data)
+
+
+def WaitForClient():
+	hostMACAddress = "B8:27:EB:A6:9E:7E" #Jesus
+    	#hostMACAddress = "B8:27:EB:AB:1C:2B" #Josh
+    	#hostMACAddress = "B8:27:EB:1A:E0:6F" #Nicke
+	#hostMACAddress = ""
+	subprocess.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
+	port = 4
+	backlog = 10
+	size = 1024
+	s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+	name = bluetooth.read_local_bdaddr()
+	print("Host: %s" % name[0])
+	if name[0] == "B8:27:EB:1A:E0:6F":
+		print("Nicke")
+		hostMACAddress = "B8:27:EB:1A:E0:6F"
+	else:
+		print("Jesus")
+		hostMACAddress = "B8:27:EB:A6:9E:7E"
+	s.bind((hostMACAddress, port))
+	print("Listening")
+	s.listen(backlog)
+	try:
+		client, clientInfo = s.accept()
+		while 1:
+            test = input("h or not: ")
+			print("Connected", clientInfo)
+	    #threading.Thread(target=recv, daemon=True).start()
+		#
+			client.send(test)
+			data = client.recv(size)
+			if not data:
+				print ("Wrong Data %s" % data)
+			print ("received: %s" % data)
+	except:
+		print("Closing socket")
+            #client.close()
+		s.close()
+
+def main():
+    WaitForClient()
+    s.close()
+
+threading.Thread(target=main).start()
